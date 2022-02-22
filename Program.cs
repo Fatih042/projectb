67e0f1fcd2cf9be12630f760c302a69a0222e558
Fatih042 <93191470+Fatih042@users.noreply.github.com>
2022-02-22 11:28:55 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int a = 3;
+        int b = 4;
+        bool c = true;
+        string result = "";
+        if (a < b)
+        {
+            result += "A";
+        }
+        else
+        {
+            result += "B";
+        }
+
+        if (a % 2 == 0 || b % 2 == 0)
+        {
+            result += "C";
+        }
+        if (!c)
+        {
+            result += "D";
+        }
+        if (a != b)
+        {
+            result += "E";
+            if ((a + b * a) > 15)
+            {
+                result += "F";
+            }
+            else
+            {
+                result += "G";
+            }
+        }
+    }
+}
